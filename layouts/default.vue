59940bb7849bdd842cd29ef3c65effcddecfe9bb
Freeptom <32514405+Freeptom@users.noreply.github.com>
2020-09-12 11:52:31 +0100

    styling to intro section, adding decoration

---
+++ layouts/default.vue
@@ -0,0 +1,183 @@
+<template>
+  <div>
+    <header>
+      <div class="wrapper">
+        <div class="navbar">
+          <nuxt-link to="/">tom the dev</nuxt-link>
+          <nav>
+            <ul>
+              <li>
+                <nuxt-link to="#">about</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="#">portfolio</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="#">blog</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="#" class="cta cta--small">contact me</nuxt-link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-family: 'CircularStd' 'Segoe UI', 'Candara', 'Bitstream Vera Sans',
+    'DejaVu Sans', 'Bitsream Vera Sans', 'Trebuchet MS', 'Verdana',
+    'Verdana Ref', sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+@font-face {
+  font-family: 'CircularStd';
+  src: url('~assets/fonts/CircularStd-Book.ttf') format('truetype');
+  font-weight: 300;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'CircularStd';
+  src: url('~assets/fonts/CircularStd-BookItalic.ttf') format('truetype');
+  font-weight: 300;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: 'CircularStd';
+  src: url('~assets/fonts/CircularStd-Medium.ttf') format('truetype');
+  font-weight: 500;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'CircularStd';
+  src: url('~assets/fonts/CircularStd-Bold.ttf') format('truetype');
+  font-weight: bold;
+  font-style: normal;
+}
+
+body {
+  font-family: 'CircularStd', Georgia, serif;
+  width: 100%;
+}
+
+h1 {
+  font-weight: bold;
+}
+
+em {
+  font-style: italic;
+}
+
+strong em {
+  font-weight: bold;
+  font-style: italic;
+}
+
+.full-bleed {
+  width: 100vw;
+  margin-left: calc(50% - 50vw);
+}
+
+.wrapper {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1040px;
+  width: 90%;
+}
+
+header {
+  background: white;
+  padding: 2.5rem 0;
+}
+
+.navbar {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: 300;
+  a {
+    color: $dark;
+    text-decoration: none;
+  }
+  nav {
+    display: flex;
+    ul {
+      list-style-type: none;
+      padding: 0;
+      display: flex;
+      li a {
+        padding: 0.5rem;
+        &.cta--small {
+          @include cta--small;
+        }
+      }
+      @media only screen and (min-width: $desktop) {
+        padding: 1rem;
+      }
+    }
+  }
+}
+
+a.cta {
+  @include cta;
+  &--small {
+    @include cta--small;
+  }
+}
+
+section {
+  margin: 0 auto;
+  padding: 40px 0 4rem 0;
+  @media only screen and (min-width: $lg-mobile) {
+    padding: 60px 0 6rem 0;
+  }
+  @media only screen and (min-width: $tablet) {
+    padding: 108px 0 12rem 0;
+  }
+  h1,
+  h2 {
+    margin-bottom: 10px;
+    color: $dark;
+    letter-spacing: 0.02rem;
+    line-height: 1.1;
+    font-weight: 400;
+    font-size: 2.4rem;
+    @media only screen and (min-width: $lg-mobile) {
+      font-size: 2.75rem;
+    }
+    @media only screen and (min-width: $desktop) {
+      font-size: 3.75rem;
+      line-height: 102%;
+    }
+  }
+}
+
+@media only screen and (min-width: $desktop) {
+  body {
+    font-size: 18px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="wrapper">
+    <section class="intro">
+      <div>
+        <h1>Finally, a website your users will love.</h1>
+        <p class="intro__sub-heading">
+          I’m Tom – builder of <strong>beautiful</strong>,
+          <strong>bespoke</strong> and <strong>accessible</strong> websites for
+          businesses big and small.
+        </p>
+        <nuxt-link to="#" class="cta">View my Work</nuxt-link>
+      </div>
+      <div class="intro__image-wrapper">
+        <img src="~/assets/images/laptop.svg" />
+      </div>
+    </section>
+
+    <div class="wrapper">
+      <section class="how full-bleed">
+        <div class="wrapper">
+          <h2>How I can help you get more customers</h2>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Tom the Web Developer'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'The portfolio for freelance web developer, Tom Pearson'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.intro {
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  h1 {
+    max-width: 40ch;
+  }
+  @media only screen and (min-width: $desktop) {
+    flex-wrap: nowrap;
+    h1 {
+      max-width: 20ch;
+    }
+  }
+  &__sub-heading {
+    font-weight: 300;
+    font-size: 1.2rem;
+    max-width: 45ch;
+    color: $grey1;
+    letter-spacing: 0.02rem;
+    margin: 0 0 1.75rem 0;
+    @media only screen and (min-width: $desktop) {
+      font-size: 1.5rem;
+      margin: 0 0 2.5rem 0;
+    }
+  }
+  &__image-wrapper {
+    position: relative;
+    width: auto;
+    @media only screen and (min-width: $tablet) {
+      margin: 4rem auto;
+    }
+    @media only screen and (min-width: $desktop) {
+      margin: 0;
+    }
+    img {
+      z-index: 1;
+      align-self: center;
+      width: 100%;
+      max-width: 200px;
+      height: auto;
+      display: none;
+      @media only screen and (min-width: $tablet) {
+        max-width: 385px;
+        display: block;
+        align-self: flex-end;
+      }
+    }
+    &::before {
+      content: '';
+      position: absolute;
+      top: -40px;
+      right: -40px;
+      z-index: -1;
+      border: 10px solid $primary-color;
+      height: 150px;
+      width: 150px;
+      border-radius: 50%;
+      @media only screen and (min-width: $tablet) {
+        height: 272px;
+        width: 272px;
+        top: -100px;
+        right: -80px;
+        border: 20px solid $primary-color;
+      }
+    }
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 15vh;
+    right: 25vw;
+    z-index: -1;
+    background: $neutral;
+    height: 54px;
+    width: 54px;
+    border-radius: 50%;
+  }
+}
+
+.how {
+  background: $primary-color;
+  position: relative;
+  &::after {
+    content: '';
+    position: absolute;
+    top: -10vh;
+    right: 12vw;
+    z-index: -1;
+    background: $accent2;
+    height: 126px;
+    width: 126px;
+    border-radius: 50%;
+  }
+}
+</style>
